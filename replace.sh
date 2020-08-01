cfc8b2b79524dd6ef8db80964b1444730a8213f7
Xan <git@xanban.xyz>
2020-08-01 23:25:49 +0800

    edited README.md

--- replace.sh
+++ replace.sh
@@ -6,15 +6,16 @@
 # Made by Xan#7777
 
 # MD Image Replace
-sed -i 's#!\[](#<p align="center"><img src="#g' "$1"
+sed -i -E 's|\[!\[\]\(.*\)\]\(|<p align="center"><img src="|g' "$1"
+sed -i -E 's|!\[\]\(|<p align="center"><img src="|g' "$1"
 
 # png replace
-sed -i 's#png)#png"><\/p>#g' "$1"
-sed -i 's#PNG)#PNG"><\/p>#g' "$1"
+sed -i 's|png)|png"><\/p>|g' "$1"
+sed -i 's|PNG)|PNG"><\/p>|g' "$1"
 
 # callout check
-sed -i 's#<callout info>#<c><p class="callout info">#g' "$1"
-sed -i 's#<callout success>#<c><p class="callout success">#g' "$1"
-sed -i 's#<callout warning>#<c><p class="callout warning">#g' "$1"
-sed -i 's#<callout danger>#<c><p class="callout danger">#g' "$1"
-sed -i 's#<\/callout>#<\/p><\/c>#g' "$1"
+sed -i 's|<callout info>|<c><p class="callout info">|g' "$1"
+sed -i 's|<callout success>|<c><p class="callout success">|g' "$1"
+sed -i 's|<callout warning>|<c><p class="callout warning">|g' "$1"
+sed -i 's|<callout danger>|<c><p class="callout danger">|g' "$1"
+sed -i 's|<\/callout>|<\/p><\/c>|g' "$1"
