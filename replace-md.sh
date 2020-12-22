e6d204ca3808560c9c97842ea0819de00ef4723a
XantherBanter <git@xanban.xyz>
2020-12-23 03:03:40 +0800

    recursive callout replace

--- replace-md.sh
+++ replace-md.sh
@@ -6,16 +6,21 @@
 # Made by Xan#7777
 
 # MD Image Replace
-sed -i -E 's|\[https://i\.imgur\.com|\![https://i\.imgur\.com|g' "$1"
-sed -i -E 's|<p align="center"><img src="|!\[\]\(|g' "$1"
-sed -i 's|png"><\/p>)|png)|g' "$1"
-sed -i 's|PNG"><\/p>)|PNG)|g' "$1"
-sed -i 's|jpg"><\/p>)|jpg)|g' "$1"
-sed -i 's|JPG"><\/p>)|JPG)|g' "$1"
+find . -type f -name "*.md" -print0 | xargs -0 sed -i -E 's|\[https://i\.imgur\.com|\![https://i\.imgur\.com|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i -E 's|<p align="center"><img src="|!\[\]\(|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|png"><\/p>)|png)|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|PNG"><\/p>)|PNG)|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|jpg"><\/p>)|jpg)|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|JPG"><\/p>)|JPG)|g'
 
 # callout check
-# sed -i 's|<callout info>|<c><p class="callout info">|g' "$1"
-# sed -i 's|<callout success>|<c><p class="callout success">|g' "$1"
-# sed -i 's|<callout warning>|<c><p class="callout warning">|g' "$1"
-# sed -i 's|<callout danger>|<c><p class="callout danger">|g' "$1"
-# sed -i 's|<\/callout>|<\/p><\/c>|g' "$1"
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<c><p class="callout info">|::: info\n|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<c><p class="callout success">|::: success\n|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<c><p class="callout warning">|::: warning\n|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<c><p class="callout danger">|::: danger\n|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<\/p><\/c>|\n:::|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<p class="callout info">|::: info\n|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<p class="callout success">|::: success\n|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<p class="callout warning">|::: warning\n|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<p class="callout danger">|::: danger\n|g'
+find . -type f -name "*.md" -print0 | xargs -0 sed -i 's|<\/p>|\n:::|g'
